0c4b7221889138c5266b94475a411565191a7f4b
damonot <laptop+damonot@users.noreply.github.com>
2020-07-13 16:41:24 -0400

    tweak getrepo download pathing

--- scripts/oterogetrepo.py
+++ scripts/oterogetrepo.py
@@ -7,21 +7,20 @@ Created on Wed Nov  6 20:57:54 2019
 import os
 from git import Repo
 
-# downloads the github 'che' repo
 def download(gitURL):
     response = input('Do you want to download '+gitURL+'?\n[y]/n\n')
     if(response == 'y'):
         print('Attempting download of git repo \'' + gitURL + '\'')    
         folderName = gitURL.rsplit('/', 1)[-1] # last part of gitURL
         
-        folPath = os.getcwd() + '\\'+folderName
+        folPath = os.getcwd() + '\\input\\'+folderName
         
         try: # download repo to machine
-            os.mkdir(folPath) # create new folder from current working direc
+            os.mkdir(folPath) # create new folder from cwd
             print("Directory "+ folPath+" created.") 
             print("Downloading "+folderName+" repository to current working "+
                   "directory (this may take a while).")
-            Repo.clone_from(gitURL, folPath) # download github repo to chePath
+            Repo.clone_from(gitURL, folPath) # download github repo to path
             print("Download complete.\n")
         except FileExistsError: # folder already exists
             print("Directory " + folPath+  " already exists.\n"+
