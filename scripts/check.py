b2edf17c3e3248f10214ceeeff316b65ed50358a
damonot <laptop+damonot@users.noreply.github.com>
2020-07-17 02:36:41 -0400

    renamed variables for clarity

--- scripts/check.py
+++ scripts/check.py
@@ -1,18 +1,18 @@
 # Checks if an output folder for the repo has already been generated. Generates one if not.
 import os
 
-def folder(verbose, repo):
+def folder(verbose, folder):
   #if verbose:
   #  print("Checking if {} exists in 'output' folder...".format(repo)) 
-  path = os.getcwd() + "\\output\\" + repo
+  path = os.getcwd() + "\\output\\" + folder
   
   if not os.path.exists(path):
     os.makedirs(path)
     if verbose:
-      print("\t{} created in 'output' folder.".format(repo))
+      print("\t{} created in 'output' folder.".format(folder))
   else:
     if verbose:
-      print("\t{} folder already exists in 'output'".format(repo))
+      print("\t{} folder already exists in 'output'".format(folder))
 
 def fyle(verbose, repo, fyle):
   if os.path.isfile(fyle):
