9707538552f19e16e47900f8acb31f3c1ce796b5
Damon <labs+damonot@users.noreply.github.com>
2020-07-20 15:13:49 -0400

    changed pip install commands

--- scripts/startbiconvenv.bat
+++ scripts/startbiconvenv.bat
@@ -1,4 +1,5 @@
 CALL bicon-venv\Scripts\activate.bat
-CALL pip install -r requirements_bicon-venv.txt
+CALL pip install bicon
+CALL pip install openpyxl
 CALL echo bicon-venv activated
 CALL python bicluster.py %1 %2 %3
